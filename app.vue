bf9832ecdcb441475828e802134fab711212fb53
ryuhei373 <mail@ryuhei.dev>
2023-12-09 03:13:39 +0900

    useSeoMetaを追加

--- app.vue
+++ app.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: '373.3',
+  description: 'webアプリケーション開発者。仕事ではフルスタック気味に従事。個人としてはHTMLとCSSが好き。',
+  twitterCard: 'summary',
+  twitterSite: '@373_3',
+  twitterTitle: '373.3',
+  ogTitle: '373.3',
+  ogType: 'website',
+  ogSiteName: '373.3',
+  ogDescription: 'webアプリケーション開発者。仕事ではフルスタック気味に従事。個人としてはHTMLとCSSが好き。',
+  ogImage: 'https://github.com/ryuhei373.png',
+  ogLocale: 'ja_JP',
+  ogUrl: 'https://373.deno.dev/'
+});
+</script>
+
 <template>
   <div class="container mx-auto max-w-screen-md p-4 md:px-0">
     <!-- Markup shared across all pages, ex: NavBar -->
