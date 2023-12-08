6b15d19ea109e7602e0b3f4483533196276b43b5
ryuhei373 <mail@ryuhei.dev>
2023-12-09 03:02:51 +0900

    useAsyncDataでもってきてContentRendererに渡す形式に変更

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
+const { path } = useRoute();
+const { data } = await useAsyncData('page-data', () => queryContent().where({ _path: path }).findOne())
 </script>
 
 
 <template>
-    <ContentDoc v-slot="{ doc }">
+    <!-- <ContentDoc v-slot="{ doc }">
         <article class="mt-8">
             <h1 class="text-3xl font-bold">{{ doc.title }}</h1>
             <PostedDate :created-at="doc.createdAt" />
             <ContentRenderer :value="doc" />
         </article>
-    </ContentDoc>
+    </ContentDoc> -->
+    <article class="mt-8">
+        <ContentRenderer v-if="data" :value="data">
+            <h1 class="text-3xl font-bold">{{ data.title }}</h1>
+            <PostedDate :created-at="data.createdAt" />
+            <ContentRendererMarkdown :value="data" />
+        </ContentRenderer>
+    </article>
 </template>
