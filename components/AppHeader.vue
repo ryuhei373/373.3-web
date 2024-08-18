0feddb97f8c519d1edb5205fbdc27810649636ad
ryuhei373 <mail@ryuhei.dev>
2024-08-18 23:47:53 +0900

    add dark theme

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,7 +3,8 @@
 
 <template>
     <header class="mx-auto flex items-center">
-        <nuxt-link to="/" class="mb-0 flex items-center font-medium text-orange hover:text-orange-light">
+        <nuxt-link to="/"
+            class="mb-0 flex items-center font-medium text-orange hover:text-orange-light dark:text-orange-light dark:hover:text-orange">
             <img src="https://github.com/ryuhei373.png" width="32" height="32" alt="ryuhei373"
                 class="mr-3 inline-block rounded-full">
             <span class="text-xl font-bold">373.3</span>
--- components/content/ProseCodeInline.vue
+++ components/content/ProseCodeInline.vue
@@ -1,3 +1,4 @@
 <template>
-    <code class="bg-base-50 border border-base-100 border-solid px-1.5 py-0.5 text-sm rounded"><slot /></code>
+    <code
+        class="bg-base-50 dark:bg-base-800 border-base-100 dark:border-base-600 border border-solid px-1.5 py-0.5 text-sm rounded"><slot /></code>
 </template>
